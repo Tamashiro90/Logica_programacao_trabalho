e86220a6d5eebe70904dd1e5a5370385f745fc39
Tamashiro90 <leandro.tamashiro90@gmail.com>
2024-09-26 18:28:12 -0300

    Update 1002.js

--- 1002.js
+++ 1002.js
@@ -1,9 +1,9 @@
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
 
-var raio=parseFloat(lines.shift())
-var area=parseFloat(lines.shift())
+var raio=parseFloat(lines.shift());
+var area=parseFloat(lines.shift());
 
-area=(3.14159*(raio*raio))
+area=(3.14159*(raio*raio));
 
-console.log("A="+area.toFixed(4))
+console.log("A="+area.toFixed(4));
